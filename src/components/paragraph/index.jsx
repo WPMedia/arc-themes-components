155aa7f77a8964ac7468161cb02e3bac4596c134
Jack Howard <jack.howard@washpost.com>
2022-01-27 09:03:21 -0600

    test: Add string to break tests

--- src/components/paragraph/index.jsx
+++ src/components/paragraph/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Paragraph = ({ children }) => (
-	<p className="c-paragraph">{children}</p>
+	<p className="c-paragraph">{children}f</p>
 );
 
 Paragraph.propTypes = {
