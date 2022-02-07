bee716b65c6dadf1111410db81d80476c41d1c06
Jack Howard <jackhowa@fastmail.com>
2022-02-07 10:14:04 -0600

    ci: add comment to test perms

--- jest/fileTransformer.js
+++ jest/fileTransformer.js
@@ -1,5 +1,6 @@
 const path = require('path');
 
+// add comment to test perms
 module.exports = {
   process(src, filename) {
     return `module.exports = ${JSON.stringify(path.basename(filename))};`;
