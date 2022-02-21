404530e27eac82bda0a29e9979207a3564ff2f66
Brent Miller <brent.miller@washport.com>
2022-02-21 14:43:04 -0600

    Stack integration and tests

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 /* eslint import/order: ["error", {"alphabetize": {"order": "asc", "caseInsensitive": true}}] */
 // inject imports after this comment and alphabetize them
-import Stack from './src/components/stack';
 import Heading from './src/components/headings/heading';
 import HeadingSection from './src/components/headings/section';
 import Link from './src/components/link';
 import Paragraph from './src/components/paragraph';
+import Stack from './src/components/stack';
 
 export {
-	Stack,
 	Heading,
 	HeadingSection,
 	Link,
 	Paragraph,
+	Stack,
 };
