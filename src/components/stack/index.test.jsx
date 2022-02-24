113ee85d66254f3018c6ffcd0994f71d878a16c4
Brent Miller <brent.miller@washport.com>
2022-02-24 14:43:22 -0600

    Pr updates feb 24th

--- src/components/stack/index.test.jsx
+++ src/components/stack/index.test.jsx
@@ -1,353 +1,316 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import '@testing-library/jest-dom';
-import Stack from '.';
+import React from "react";
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Stack from ".";
 
-describe('Stack', () => {
-	it('should render only one child div', () => {
+describe("Stack", () => {
+	it("should render only one child div", () => {
 		const { container } = render(
 			<Stack>
-				<div style={{ height: '100px', width: '200px' }}>
+				<div style={{ height: "100px", width: "200px" }}>
 					<img alt="image 1" src="https://picsum.photos/200/100" />
 				</div>
-			</Stack>,
+			</Stack>
 		);
-		expect(container.querySelectorAll('.c-stack div'))
-			.toHaveLength(1);
+		expect(container.querySelectorAll(".c-stack div")).toHaveLength(1);
 	});
 
-	it('should render only one child div with divider', () => {
-		const { container } = render(
-			<Stack divider>
-				<div style={{ height: '100px', width: '200px' }}>
-					<img alt="image 1" src="https://picsum.photos/200/100" />
-				</div>
-			</Stack>,
-		);
-		expect(container.querySelectorAll('.c-stack div'))
-			.toHaveLength(1);
-
-		expect(container.querySelectorAll('.c-stack hr'))
-			.toHaveLength(1);
-	});
-
-	it('should render only two child divs', () => {
+	it("should render only two child divs", () => {
 		const { container } = render(
 			<Stack>
-				<div style={{ height: '100px', width: '200px' }}>
+				<div style={{ height: "100px", width: "200px" }}>
 					<img alt="image 1" src="https://picsum.photos/200/100" />
 				</div>
-				<div style={{ height: '100px', width: '200px' }}>
+				<div style={{ height: "100px", width: "200px" }}>
 					<img alt="image 2" src="https://loremflickr.com/200/100" />
 				</div>
-			</Stack>,
+			</Stack>
 		);
-		expect(container.querySelectorAll('.c-stack div'))
-			.toHaveLength(2);
+		expect(container.querySelectorAll(".c-stack div")).toHaveLength(2);
 	});
 
-	it('should render vertical layout by default', () => {
+	it("should render vertical layout by default", () => {
 		const { container } = render(
 			<Stack>
-				<div style={{ height: '100px', width: '200px' }}>
+				<div style={{ height: "100px", width: "200px" }}>
 					<img alt="image 1" src="https://picsum.photos/200/100" />
 				</div>
-				<div style={{ height: '100px', width: '200px' }}>
+				<div style={{ height: "100px", width: "200px" }}>
 					<img alt="image 2" src="https://loremflickr.com/200/100" />
 				</div>
-			</Stack>,
+			</Stack>
 		);
-		expect(container.querySelector('.c-stack')
-			.getAttribute('data-style-direction'))
-			.toBe('vertical');
+		expect(container.querySelector(".c-stack").getAttribute("data-style-direction")).toBe(
+			"vertical"
+		);
 	});
 
-	it('should apply custom classes in the class attribute', () => {
+	it("should apply custom classes in the class attribute", () => {
 		const { container } = render(
 			<Stack additionalClassNames="test-class">
-				<div style={{ height: '100px', width: '200px' }}>
+				<div style={{ height: "100px", width: "200px" }}>
 					<img alt="image 1" src="https://picsum.photos/200/100" />
 				</div>
-				<div style={{ height: '100px', width: '200px' }}>
+				<div style={{ height: "100px", width: "200px" }}>
 					<img alt="image 2" src="https://loremflickr.com/200/100" />
 				</div>
-			</Stack>,
+			</Stack>
 		);
-		expect(container.querySelector('.c-stack')
-			.getAttribute('class'))
-			.toContain('test-class');
+		expect(container.querySelector(".c-stack").getAttribute("class")).toContain("test-class");
 	});
 
-	it('should render vertical layout when explicitly specified', () => {
+	it("should render vertical layout when explicitly specified", () => {
 		const { container } = render(
 			<Stack direction="vertical">
-				<div style={{ height: '100px', width: '200px' }}>
+				<div style={{ height: "100px", width: "200px" }}>
 					<img alt="image 1" src="https://picsum.photos/200/100" />
 				</div>
-				<div style={{ height: '100px', width: '200px' }}>
+				<div style={{ height: "100px", width: "200px" }}>
 					<img alt="image 2" src="https://loremflickr.com/200/100" />
 				</div>
-			</Stack>,
+			</Stack>
 		);
-		expect(container.querySelector('.c-stack')
-			.getAttribute('data-style-direction'))
-			.toBe('vertical');
+		expect(container.querySelector(".c-stack").getAttribute("data-style-direction")).toBe(
+			"vertical"
+		);
 	});
 
-	it('should render horizontal layout when explicitly specified', () => {
+	it("should render horizontal layout when explicitly specified", () => {
 		const { container } = render(
 			<Stack direction="horizontal">
-				<div style={{ height: '100px', width: '200px' }}>
+				<div style={{ height: "100px", width: "200px" }}>
 					<img alt="image 1" src="https://picsum.photos/200/100" />
 				</div>
-				<div style={{ height: '100px', width: '200px' }}>
+				<div style={{ height: "100px", width: "200px" }}>
 					<img alt="image 2" src="https://loremflickr.com/200/100" />
 				</div>
-			</Stack>,
+			</Stack>
 		);
-		expect(container.querySelector('.c-stack')
-			.getAttribute('data-style-direction'))
-			.toBe('horizontal');
+		expect(container.querySelector(".c-stack").getAttribute("data-style-direction")).toBe(
+			"horizontal"
+		);
 	});
 
-	it('should render wrap=nowrap when by default', () => {
+	it("should render wrap=nowrap when by default", () => {
 		const { container } = render(
 			<Stack direction="horizontal">
-				<div style={{ height: '100px', width: '200px' }}>
+				<div style={{ height: "100px", width: "200px" }}>
 					<img alt="image 1" src="https://picsum.photos/200/100" />
 				</div>
-				<div style={{ height: '100px', width: '200px' }}>
+				<div style={{ height: "100px", width: "200px" }}>
 					<img alt="image 2" src="https://loremflickr.com/200/100" />
 				</div>
-			</Stack>,
+			</Stack>
 		);
 
-		expect(container.querySelector('.c-stack')
-			.getAttribute('data-style-wrap'))
-			.toBe('nowrap');
+		expect(container.querySelector(".c-stack").getAttribute("data-style-wrap")).toBe("nowrap");
 	});
 
-	it('should render  wrap=wrap when by specified', () => {
+	it("should render  wrap=wrap when by specified", () => {
 		const { container } = render(
 			<Stack direction="horizontal" wrap="wrap">
-				<div style={{ height: '100px', width: '200px' }}>
+				<div style={{ height: "100px", width: "200px" }}>
 					<img alt="image 1" src="https://picsum.photos/200/100" />
 				</div>
-				<div style={{ height: '100px', width: '200px' }}>
+				<div style={{ height: "100px", width: "200px" }}>
 					<img alt="image 2" src="https://loremflickr.com/200/100" />
 				</div>
-			</Stack>,
+			</Stack>
 		);
 
-		expect(container.querySelector('.c-stack')
-			.getAttribute('data-style-wrap'))
-			.toBe('wrap');
+		expect(container.querySelector(".c-stack").getAttribute("data-style-wrap")).toBe("wrap");
 	});
 
-	it('should render  wrap=wrap-reverse when by specified', () => {
+	it("should render  wrap=wrap-reverse when by specified", () => {
 		const { container } = render(
 			<Stack direction="horizontal" wrap="wrap-reverse">
-				<div style={{ height: '100px', width: '200px' }}>
+				<div style={{ height: "100px", width: "200px" }}>
 					<img alt="image 1" src="https://picsum.photos/200/100" />
 				</div>
-				<div style={{ height: '100px', width: '200px' }}>
+				<div style={{ height: "100px", width: "200px" }}>
 					<img alt="image 2" src="https://loremflickr.com/200/100" />
 				</div>
-			</Stack>,
+			</Stack>
 		);
 
-		expect(container.querySelector('.c-stack')
-			.getAttribute('data-style-wrap'))
-			.toBe('wrap-reverse');
+		expect(container.querySelector(".c-stack").getAttribute("data-style-wrap")).toBe(
+			"wrap-reverse"
+		);
 	});
 
-	it('should render unset alignment by default', () => {
+	it("should render unset alignment by default", () => {
 		const { container } = render(
 			<Stack>
-				<div style={{ height: '100px', width: '200px' }}>
+				<div style={{ height: "100px", width: "200px" }}>
 					<img alt="image 1" src="https://picsum.photos/200/100" />
 				</div>
-				<div style={{ height: '100px', width: '200px' }}>
+				<div style={{ height: "100px", width: "200px" }}>
 					<img alt="image 2" src="https://loremflickr.com/200/100" />
 				</div>
-			</Stack>,
+			</Stack>
 		);
-		expect(container.querySelector('.c-stack')
-			.getAttribute('data-style-alignment'))
-			.toBe('unset');
+		expect(container.querySelector(".c-stack").getAttribute("data-style-alignment")).toBe("unset");
 	});
 
-	it('should render start alignment when explicitly specified', () => {
+	it("should render start alignment when explicitly specified", () => {
 		const { container } = render(
 			<Stack alignment="start">
-				<div style={{ height: '100px', width: '200px' }}>
+				<div style={{ height: "100px", width: "200px" }}>
 					<img alt="image 1" src="https://picsum.photos/200/100" />
 				</div>
-				<div style={{ height: '100px', width: '200px' }}>
+				<div style={{ height: "100px", width: "200px" }}>
 					<img alt="image 2" src="https://loremflickr.com/200/100" />
 				</div>
-			</Stack>,
+			</Stack>
 		);
-		expect(container.querySelector('.c-stack')
-			.getAttribute('data-style-alignment'))
-			.toBe('start');
+		expect(container.querySelector(".c-stack").getAttribute("data-style-alignment")).toBe("start");
 	});
 
-	it('should render center alignment when explicitly specified', () => {
+	it("should render center alignment when explicitly specified", () => {
 		const { container } = render(
 			<Stack alignment="center">
-				<div style={{ height: '100px', width: '200px' }}>
+				<div style={{ height: "100px", width: "200px" }}>
 					<img alt="image 1" src="https://picsum.photos/200/100" />
 				</div>
-				<div style={{ height: '100px', width: '200px' }}>
+				<div style={{ height: "100px", width: "200px" }}>
 					<img alt="image 2" src="https://loremflickr.com/200/100" />
 				</div>
-			</Stack>,
+			</Stack>
 		);
-		expect(container.querySelector('.c-stack')
-			.getAttribute('data-style-alignment'))
-			.toBe('center');
+		expect(container.querySelector(".c-stack").getAttribute("data-style-alignment")).toBe("center");
 	});
 
-	it('should render end alignment when explicitly specified', () => {
+	it("should render end alignment when explicitly specified", () => {
 		const { container } = render(
 			<Stack alignment="end">
-				<div style={{ height: '100px', width: '200px' }}>
+				<div style={{ height: "100px", width: "200px" }}>
 					<img alt="image 1" src="https://picsum.photos/200/100" />
 				</div>
-				<div style={{ height: '100px', width: '200px' }}>
+				<div style={{ height: "100px", width: "200px" }}>
 					<img alt="image 2" src="https://loremflickr.com/200/100" />
 				</div>
-			</Stack>,
+			</Stack>
 		);
-		expect(container.querySelector('.c-stack')
-			.getAttribute('data-style-alignment'))
-			.toBe('end');
+		expect(container.querySelector(".c-stack").getAttribute("data-style-alignment")).toBe("end");
 	});
 
-	it('should render start justification when explicitly specified', () => {
+	it("should render start justification when explicitly specified", () => {
 		const { container } = render(
 			<Stack justification="start">
-				<div style={{ height: '100px', width: '200px' }}>
+				<div style={{ height: "100px", width: "200px" }}>
 					<img alt="image 1" src="https://picsum.photos/200/100" />
 				</div>
-				<div style={{ height: '100px', width: '200px' }}>
+				<div style={{ height: "100px", width: "200px" }}>
 					<img alt="image 2" src="https://loremflickr.com/200/100" />
 				</div>
-			</Stack>,
+			</Stack>
 		);
-		expect(container.querySelector('.c-stack')
-			.getAttribute('data-style-justification'))
-			.toBe('start');
+		expect(container.querySelector(".c-stack").getAttribute("data-style-justification")).toBe(
+			"start"
+		);
 	});
 
-	it('should render center justification when explicitly specified', () => {
+	it("should render center justification when explicitly specified", () => {
 		const { container } = render(
 			<Stack justification="center">
-				<div style={{ height: '100px', width: '200px' }}>
+				<div style={{ height: "100px", width: "200px" }}>
 					<img alt="image 1" src="https://picsum.photos/200/100" />
 				</div>
-				<div style={{ height: '100px', width: '200px' }}>
+				<div style={{ height: "100px", width: "200px" }}>
 					<img alt="image 2" src="https://loremflickr.com/200/100" />
 				</div>
-			</Stack>,
+			</Stack>
 		);
-		expect(container.querySelector('.c-stack')
-			.getAttribute('data-style-justification'))
-			.toBe('center');
+		expect(container.querySelector(".c-stack").getAttribute("data-style-justification")).toBe(
+			"center"
+		);
 	});
 
-	it('should render end justification when explicitly specified', () => {
+	it("should render end justification when explicitly specified", () => {
 		const { container } = render(
 			<Stack justification="end">
-				<div style={{ height: '100px', width: '200px' }}>
+				<div style={{ height: "100px", width: "200px" }}>
 					<img alt="image 1" src="https://picsum.photos/200/100" />
 				</div>
-				<div style={{ height: '100px', width: '200px' }}>
+				<div style={{ height: "100px", width: "200px" }}>
 					<img alt="image 2" src="https://loremflickr.com/200/100" />
 				</div>
-			</Stack>,
+			</Stack>
 		);
-		expect(container.querySelector('.c-stack')
-			.getAttribute('data-style-justification'))
-			.toBe('end');
+		expect(container.querySelector(".c-stack").getAttribute("data-style-justification")).toBe(
+			"end"
+		);
 	});
 
-	it('should render custom gap when explicitly specified', () => {
+	it("should render custom gap when explicitly specified", () => {
 		const { container } = render(
 			<Stack gap="15px">
-				<div style={{ height: '100px', width: '200px' }}>
+				<div style={{ height: "100px", width: "200px" }}>
 					<img alt="image 1" src="https://picsum.photos/200/100" />
 				</div>
-				<div style={{ height: '100px', width: '200px' }}>
+				<div style={{ height: "100px", width: "200px" }}>
 					<img alt="image 2" src="https://loremflickr.com/200/100" />
 				</div>
-			</Stack>,
+			</Stack>
 		);
-		expect(container.querySelector('.c-stack')
-			.getAttribute('style'))
-			.toBe('--c-stack-gap: 15px;');
+		expect(container.querySelector(".c-stack").getAttribute("style")).toBe("--c-stack-gap: 15px;");
 	});
 
-	it('should not render dividers when not specified', () => {
+	it("should not render dividers when not specified", () => {
 		const { container } = render(
 			<Stack>
-				<div style={{ height: '100px', width: '200px' }}>
+				<div style={{ height: "100px", width: "200px" }}>
 					<img alt="image 1" src="https://picsum.photos/200/100" />
 				</div>
-				<div style={{ height: '100px', width: '200px' }}>
+				<div style={{ height: "100px", width: "200px" }}>
 					<img alt="image 2" src="https://loremflickr.com/200/100" />
 				</div>
-			</Stack>,
+			</Stack>
 		);
-		expect(container.querySelectorAll('.c-stack hr'))
-			.toHaveLength(0);
+		expect(container.querySelectorAll(".c-stack hr")).toHaveLength(0);
 	});
 
-	it('should render dividers when explicitly specified', () => {
+	it("should render dividers when explicitly specified", () => {
 		const { container } = render(
 			<Stack divider>
-				<div style={{ height: '100px', width: '200px' }}>
+				<div style={{ height: "100px", width: "200px" }}>
 					<img alt="image 1" src="https://picsum.photos/200/100" />
 				</div>
-				<div style={{ height: '100px', width: '200px' }}>
+				<div style={{ height: "100px", width: "200px" }}>
 					<img alt="image 2" src="https://loremflickr.com/200/100" />
 				</div>
-			</Stack>,
+			</Stack>
 		);
-		expect(container.querySelectorAll('.c-stack hr'))
-			.toHaveLength(2);
+		expect(container.querySelectorAll(".c-stack hr")).toHaveLength(1);
 	});
 
-	it('should render a flex flextype by default', () => {
+	it("should render a flex flextype by default", () => {
 		const { container } = render(
 			<Stack>
-				<div style={{ height: '100px', width: '200px' }}>
+				<div style={{ height: "100px", width: "200px" }}>
 					<img alt="image 1" src="https://picsum.photos/200/100" />
 				</div>
-				<div style={{ height: '100px', width: '200px' }}>
+				<div style={{ height: "100px", width: "200px" }}>
 					<img alt="image 2" src="https://loremflickr.com/200/100" />
 				</div>
-			</Stack>,
+			</Stack>
 		);
-		expect(container.querySelector('.c-stack')
-			.getAttribute('data-style-flextype'))
-			.toBe('flex');
+		expect(container.querySelector(".c-stack").getAttribute("data-style-flextype")).toBe("flex");
 	});
 
-	it('should render a inline-flex flextype when specified', () => {
+	it("should render a inline-flex flextype when specified", () => {
 		const { container } = render(
 			<Stack flexType="inline-flex">
-				<div style={{ height: '100px', width: '200px' }}>
+				<div style={{ height: "100px", width: "200px" }}>
 					<img alt="image 1" src="https://picsum.photos/200/100" />
 				</div>
-				<div style={{ height: '100px', width: '200px' }}>
+				<div style={{ height: "100px", width: "200px" }}>
 					<img alt="image 2" src="https://loremflickr.com/200/100" />
 				</div>
-			</Stack>,
+			</Stack>
 		);
-		expect(container.querySelector('.c-stack')
-			.getAttribute('data-style-flextype'))
-			.toBe('inline-flex');
+		expect(container.querySelector(".c-stack").getAttribute("data-style-flextype")).toBe(
+			"inline-flex"
+		);
 	});
 });
