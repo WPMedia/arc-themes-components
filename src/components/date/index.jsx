0e531e5480e292adb926d9ef29d55c6a76272be6
Jack Howard <jackhowa@fastmail.com>
2022-02-28 12:47:34 -0600

    feat: Add datetime and datestring requirements based on ticket requirements

--- src/components/date/index.jsx
+++ src/components/date/index.jsx
@@ -2,17 +2,22 @@ import PropTypes from "prop-types";
 
 const COMPONENT_CLASS_NAME = "c-date";
 
-const Date = ({ children, className }) => (
-	<div className={className ? `${COMPONENT_CLASS_NAME} ${className}` : `${COMPONENT_CLASS_NAME}`}>
-		{children}
-	</div>
+const Date = ({ className, dateString, dateTime }) => (
+	<time
+		className={className ? `${COMPONENT_CLASS_NAME} ${className}` : `${COMPONENT_CLASS_NAME}`}
+		dateTime={dateTime}
+	>
+		{dateString}
+	</time>
 );
 
 Date.propTypes = {
 	/** Class name(s) that get appended to default class name of the component */
 	className: PropTypes.string,
-	/** The text, images or any node that will be displayed within the component */
-	children: PropTypes.node.isRequired,
+	/** The human-readable text that will be displayed within the component */
+	dateString: PropTypes.string.isRequired,
+	/** The date and time of the dateString passed in. This must be a valid machine-readable datetime value */
+	dateTime: PropTypes.string.isRequired,
 };
 
 export default Date;
