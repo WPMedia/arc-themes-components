f36e051a1a7a1133a3aa3a9b7a10a0052fdd6b9a
Brent Miller <visionbegin@gmail.com>
2022-02-28 09:28:35 -0600

    Tmedia 658 component stack (#25)
    * Stack integration and tests
    
    * Stack integration and tests
    
    * Pr updates, adding flex wrap as an option
    
    * Pr updates, adding flex wrap as an option
    
    * Pr updates, adding flex wrap as an option
    
    * Update src/components/stack/_index.scss
    
    Co-authored-by: Matthew Roach <matthewroach@users.noreply.github.com>
    
    * Testing dividers
    
    * Testing dividers
    
    * Using hr for dividers, added flex alignment to options
    
    * Using hr for dividers, added flex alignment to options
    
    * Update .storybook/theme.scss
    
    Co-authored-by: nschubach <nschubach@gmail.com>
    
    * removing keys
    
    * Pr updates feb 24th
    
    * Renanming some prams
    
    * Updating docs, stories and wrap options
    
    * Removed react import from test
    
    * Updating grammer
    
    * Updating grammer
    
    Co-authored-by: Brent Miller <brent.miller@washport.com>
    Co-authored-by: Matthew Roach <matthewroach@users.noreply.github.com>
    Co-authored-by: nschubach <nschubach@gmail.com>

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ import Heading from "./src/components/headings/heading";
 import HeadingSection from "./src/components/headings/section";
 import Link from "./src/components/link";
 import Paragraph from "./src/components/paragraph";
+import Stack from "./src/components/stack";
 
-export { Heading, HeadingSection, Link, Paragraph };
+export { Heading, HeadingSection, Link, Paragraph, Stack };
