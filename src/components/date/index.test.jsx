927b118bb19707563461254d8c24a37ce54d8051
Jack Howard <jack.howard@washpost.com>
2022-03-01 10:29:21 -0600

    TMEDIA-668: date component without formatting (#28)
    * feat: Add initial date files based on generate component
    
    * feat: Add datetime and datestring requirements based on ticket requirements
    
    * feat: Clarify iso usage based on ticket feedback along with sb
    
    * fix: Use props correctly punctuated
    
    * test: Add tests for additional classes and attributes
    
    * docs: Add datetimes without timezones and without dates
    
    * docs: Just make it more clear christmas is not celebrated everywhere
    
    * docs: Fix tense of verb

---
+++ src/components/date/index.test.jsx
@@ -0,0 +1,28 @@
+import { render, screen } from "@testing-library/react";
+
+import Date from ".";
+
+describe("Date", () => {
+	it("should render date string", () => {
+		render(<Date dateTime="2018-03-05T20:00:00" dateString="March 5 at 8 at night" />);
+		const foundDate = screen.queryByText("March 5 at 8 at night");
+
+		expect(foundDate).not.toBeNull();
+		expect(foundDate).toHaveClass("c-date");
+		expect(foundDate).toHaveAttribute("datetime", "2018-03-05T20:00:00");
+	});
+	it("should have additional classes", () => {
+		render(
+			<Date
+				dateTime="2018-03-05T20:00:00"
+				dateString="March 5 at 8 at night"
+				className="test-class"
+			/>
+		);
+		const foundDate = screen.queryByText("March 5 at 8 at night");
+
+		expect(foundDate).not.toBeNull();
+		expect(foundDate).toHaveClass("c-date test-class");
+		expect(foundDate).toHaveAttribute("datetime", "2018-03-05T20:00:00");
+	});
+});
