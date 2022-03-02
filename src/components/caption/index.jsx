884f0b176bd1e16b55280947a7b7907fb85a049b
Jack Howard <jackhowa@fastmail.com>
2022-03-02 09:55:10 -0600

    fix: Use fallback null rather than using &&

--- src/components/caption/index.jsx
+++ src/components/caption/index.jsx
@@ -3,15 +3,15 @@ import PropTypes from "prop-types";
 const COMPONENT_CLASS_NAME = "c-caption";
 
 const Caption = ({ caption, className, credit, title }) =>
-	!!(title || caption || credit) && (
+	title || caption || credit ? (
 		<figcaption
 			className={className ? `${COMPONENT_CLASS_NAME} ${className}` : `${COMPONENT_CLASS_NAME}`}
 		>
-			{title && <span className="c-caption__title">{title} </span>}
-			{caption && <span className="c-caption__caption">{caption} </span>}
-			{credit && <span className="c-caption__credit">{credit}</span>}
+			{title ? <span className="c-caption__title">{title} </span> : null}
+			{caption ? <span className="c-caption__caption">{caption} </span> : null}
+			{credit ? <span className="c-caption__credit">{credit}</span> : null}
 		</figcaption>
-	);
+	) : null;
 
 Caption.propTypes = {
 	/** Class name(s) that get appended to default class name of the component */
