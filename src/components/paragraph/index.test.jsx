67c45e2d0b165725d64628bcab1ffab1c75d2f4e
Brent Miller <visionbegin@gmail.com>
2022-03-04 07:57:12 -0600

    Paragraph Component Impl (#32)
    * Paragraph Component Impl
    
    * Integrated line clamp property into token system
    
    * Updated from meeting notes
    
    * setting inline to null, when no truncation
    
    Co-authored-by: Brent Miller <brent.miller@washport.com>

--- src/components/paragraph/index.test.jsx
+++ src/components/paragraph/index.test.jsx
@@ -1,4 +1,6 @@
 import renderer from "react-test-renderer";
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom";
 import Paragraph from ".";
 
 describe("Paragraph", () => {
@@ -6,11 +8,37 @@ describe("Paragraph", () => {
 		const tree = renderer.create(<Paragraph>Paragraph Text</Paragraph>).toJSON();
 
 		expect(tree).toMatchInlineSnapshot(`
-<p
-  className="c-paragraph"
->
-  Paragraph Text
-</p>
-`);
+      <p
+        className="c-paragraph"
+        style={
+          Object {
+            "--paragraph-truncation": null,
+          }
+        }
+      >
+        Paragraph Text
+      </p>
+		`);
+	});
+
+	it("should not apply truncation by default", () => {
+		const { container } = render(<Paragraph>Paragraph Text</Paragraph>);
+		expect(container.querySelector(".c-paragraph").getAttribute("style")).toBe(null);
+	});
+
+	it("should apply truncation when specified", () => {
+		const { container } = render(<Paragraph truncationLines={3}>Paragraph Text</Paragraph>);
+		expect(container.querySelector(".c-paragraph").getAttribute("style")).toContain(
+			"--paragraph-truncation: 3;"
+		);
+	});
+
+	it("should apply custom classes in the class attribute", () => {
+		const { container } = render(
+			<Paragraph className="test-class, test-class-2">Paragraph Text</Paragraph>
+		);
+		expect(container.querySelector(".c-paragraph").getAttribute("class")).toContain(
+			"test-class, test-class-2"
+		);
 	});
 });
