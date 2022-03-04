a7da44e1217aea7c20103eb78aa0ca522e4aa908
Matthew Roach <matthewroach@users.noreply.github.com>
2022-03-04 21:14:41 +0000

    TMEDIA-688 - Icon Component (#34)
    * Add Icon component and base icons
    * Fix export order
    * Update documentation
    * Use newer storybook references

--- index.js
+++ index.js
@@ -3,8 +3,9 @@
 import Date from "./src/components/date";
 import Heading from "./src/components/headings/heading";
 import HeadingSection from "./src/components/headings/section";
+import Icon from "./src/components/icon";
 import Link from "./src/components/link";
 import Paragraph from "./src/components/paragraph";
 import Stack from "./src/components/stack";
 
-export { Date, Heading, HeadingSection, Link, Paragraph, Stack };
+export { Date, Heading, HeadingSection, Icon, Link, Paragraph, Stack };
