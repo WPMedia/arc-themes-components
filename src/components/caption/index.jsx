f0096265b95e45529b0632c2e38749a7c0a78ab7
Jack Howard <jackhowa@fastmail.com>
2022-03-04 14:11:57 -0600

    feat: Add credit format util

--- src/components/caption/index.jsx
+++ src/components/caption/index.jsx
@@ -1,33 +1,35 @@
 import PropTypes from "prop-types";
+import formatCredits from "../../utils/format-credits";
 
 const COMPONENT_CLASS_NAME = "c-caption";
 
-const Caption = ({ caption, className, credit, title }) =>
-	title || caption || credit ? (
+const Caption = ({ caption, className, credit, title }) => {
+	const formattedCredit = formatCredits(credit);
+
+	return title || caption || formattedCredit ? (
 		<figcaption
 			className={className ? `${COMPONENT_CLASS_NAME} ${className}` : `${COMPONENT_CLASS_NAME}`}
 		>
 			{title ? (
-				<span dangerouslySetInnerHTML={{ __html: title }} className="c-caption__title" />
+				<span dangerouslySetInnerHTML={{ __html: `${title} ` }} className="c-caption__title" />
 			) : null}
 			{caption ? (
-				<span dangerouslySetInnerHTML={{ __html: caption }} className="c-caption__caption" />
+				<span dangerouslySetInnerHTML={{ __html: `${caption} ` }} className="c-caption__caption" />
 			) : null}
-			{credit ? (
-				<span dangerouslySetInnerHTML={{ __html: credit }} className="c-caption__credit" />
-			) : null}
+			{formattedCredit ? <span className="c-caption__credit">{formattedCredit}</span> : null}
 		</figcaption>
 	) : null;
+};
 
 Caption.propTypes = {
 	/** Class name(s) that get appended to default class name of the component */
 	className: PropTypes.string,
-	/** Title of the caption */
+	/** Title is the first area in the component, if present */
 	title: PropTypes.node,
-	/** Caption text */
+	/** Caption is the second area in the component, if present */
 	caption: PropTypes.node,
-	/** Credit text */
-	credit: PropTypes.node,
+	/** Credit is the third area in the component, if the object contains by and affiliation */
+	credit: PropTypes.object,
 };
 
 export default Caption;
