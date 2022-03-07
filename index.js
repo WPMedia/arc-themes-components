8e133feb67795aa5d5302a3fd09f576935f0ff0d
Jack Howard <jack.howard@washpost.com>
2022-03-07 14:30:17 -0600

    Tmedia 671 caption component (#35)

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 /* eslint import/order: ["error", {"alphabetize": {"order": "asc", "caseInsensitive": true}}] */
 // inject imports after this comment and alphabetize them
+import Caption from "./src/components/caption";
 import Date from "./src/components/date";
 import Heading from "./src/components/headings/heading";
 import HeadingSection from "./src/components/headings/section";
@@ -8,5 +9,17 @@ import Image from "./src/components/image";
 import Link from "./src/components/link";
 import Paragraph from "./src/components/paragraph";
 import Stack from "./src/components/stack";
+import formatCredits from "./src/utils/format-credits";
 
-export { Date, Heading, HeadingSection, Icon, Image, Link, Paragraph, Stack };
+export {
+	Caption,
+	Date,
+	formatCredits,
+	Heading,
+	HeadingSection,
+	Icon,
+	Image,
+	Link,
+	Paragraph,
+	Stack,
+};
