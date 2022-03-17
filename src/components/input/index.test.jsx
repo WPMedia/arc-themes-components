8c284caa0aeeeba247bd46f9b2592256d92dfdfc
Jack Howard <jack.howard@washpost.com>
2022-03-17 09:15:33 -0500

    Tmedia 676 input component (#48)
    * feat: Add starter for input component
    
    * feat: Add basic functionality for input and label and types
    
    * feat: Add bem naming for handling state variations nd elements within the sass library
    
    * feat: Add style hooks for input states
    
    * feat: Add placeholder style. Fix typo
    
    * fix: Use correctmixin
    
    * fix: Remove children prop
    
    * docs: Remove example that's not visually displaying anything
    
    * docs: Remove comment
    
    * feat: Clarify input style hooks. Add label style hooks with variations
    
    * feat: Use hidden for container class name. Add variations to base classes along with focus state of input.
    
    Based on feedback https://github.com/WPMedia/arc-themes-components/pull/48#discussion_r828037013
    
    * docs: Add comment for illustrating tip area reference
    
    * fix: Fix typo from mixin component-with-states
    
    * refactor: Use default props rather than fallback param values
    
    Via https://github.com/WPMedia/arc-themes-components/pull/48#discussion_r828041225
    
    * refactor: Nest classes within parent so that they can only be used together
    
    * feat: Allow each variation to style its placeholder as well based on mocks
    
    * feat: Refactor to use the top-level parent rather than the children to style individually
    
    Based on https://github.com/WPMedia/arc-themes-components/pull/48#discussion_r828448632
    
    * refactor: Allow individual styling for each variation from the parent level
    
    Per https://github.com/WPMedia/arc-themes-components/pull/48#discussion_r828459508
    
    * test: Add test coverage rewritten in rtl
    
    * docs: Add example and intro text for the input
    
    * feat: Add focus only for input element. Add default variation as well in addition to the base formatting
    
    Based on https://github.com/WPMedia/arc-themes-components/pull/48#discussion_r828979928
    
    * fix: Use correct mixin spelling

---
+++ src/components/input/index.test.jsx
@@ -0,0 +1,61 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+
+import Input from ".";
+
+describe("Input", () => {
+	it("should render original and additional classes", () => {
+		const ORIGINAL_CLASS = "c-input";
+		const ADDITIONAL_CLASS = "additionalClass1";
+		const { container } = render(<Input label="label" name="name" className={ADDITIONAL_CLASS} />);
+		expect(container.querySelector(`.${ORIGINAL_CLASS}`)).not.toBeNull();
+		expect(container.querySelector(`.${ADDITIONAL_CLASS}`)).not.toBeNull();
+	});
+	it("renders with a label", () => {
+		render(<Input name="test" label="label text" />);
+		expect(screen.getByText("label text")).toBeInTheDocument();
+	});
+	it("renders with a tip", () => {
+		render(<Input name="test" label="label" tip="tip text" />);
+		expect(screen.getByText("tip text")).toBeInTheDocument();
+	});
+	it("renders with a placeholder", () => {
+		render(<Input name="test" label="label" placeholder="Placeholder" />);
+		expect(screen.getByPlaceholderText("Placeholder")).toBeInTheDocument();
+	});
+	it("shows an error", () => {
+		render(
+			<Input
+				name="test"
+				showDefaultError
+				label="input-label"
+				validationErrorMessage="There has been an error"
+				validationPattern="^valid$"
+			/>
+		);
+		fireEvent.change(screen.getByRole("textbox"), { target: { value: "f" } });
+		expect(screen.getByText("There has been an error")).toBeInTheDocument();
+	});
+	it("shows error after blur and then typing", () => {
+		render(<Input name="test" showDefaultError label="input-label" validationPattern="^valid$" />);
+		fireEvent.blur(screen.getByRole("textbox"));
+		fireEvent.change(screen.getByRole("textbox"), { target: { value: "f" } });
+		// shows alert without an error message
+		expect(screen.getByRole("alert")).toBeInTheDocument();
+	});
+	it("shows hidden class if hidden true", () => {
+		const { container } = render(<Input name="test" label="label" hidden />);
+		expect(container.querySelector(".c-input--hidden")).not.toBeNull();
+	});
+	it("renders autocomplete property", () => {
+		render(<Input name="test" label="label" autoComplete="on" />);
+		expect(screen.getByRole("textbox")).toHaveAttribute("autocomplete", "on");
+	});
+	it("renders default value", () => {
+		render(<Input name="test" label="label" defaultValue="default value" />);
+		expect(screen.getByRole("textbox")).toHaveValue("default value");
+	});
+	it("renders required field", () => {
+		render(<Input name="test" label="label" required />);
+		expect(screen.getByRole("textbox")).toHaveAttribute("required");
+	});
+});
