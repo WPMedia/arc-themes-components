bdc0eb468f84de83b8fd5eac688c773e74b35d66
Jack Howard <jackhowa@fastmail.com>
2022-03-18 13:25:24 -0500

    test: Add coverage for src for images

--- src/components/media-item/index.test.jsx
+++ src/components/media-item/index.test.jsx
@@ -4,7 +4,7 @@ import MediaItem from ".";
 
 describe("MediaItem", () => {
 	it("should render title, credit and caption", () => {
-		render(<MediaItem title="title" credit="credit" caption="caption" />);
+		render(<MediaItem src="/image.png" title="title" credit="credit" caption="caption" />);
 
 		expect(screen.queryByText("title")).not.toBeNull();
 
@@ -13,37 +13,57 @@ describe("MediaItem", () => {
 	});
 	it("should render string child with additional classes", () => {
 		const { container } = render(
-			<MediaItem title="title" credit="Credit" caption="caption" className="test-class" />
+			<MediaItem
+				src="/image.png"
+				title="title"
+				credit="Credit"
+				caption="caption"
+				className="test-class"
+			/>
 		);
 		expect(container.querySelector(".test-class")).not.toBeNull();
 		expect(container.querySelector(".c-media-item")).not.toBeNull();
 	});
-	it("should render null if falsy title, formatted credit and caption", () => {
-		const { container } = render(<MediaItem title="" credit="" caption="" />);
+	it("should render null if falsy title, formatted credit, caption, and src", () => {
+		const { container } = render(<MediaItem src="" title="" credit="" caption="" />);
 		expect(container.querySelector(".c-media-item")).toBeNull();
 		expect(container.querySelector(".c-media-item__title")).toBeNull();
 		expect(container.querySelector(".c-media-item__caption")).toBeNull();
 		expect(container.querySelector(".c-media-item__credit")).toBeNull();
 	});
 	it("should not render title if no title", () => {
-		const { container } = render(<MediaItem title="" credit="Credit" caption="caption" />);
+		const { container } = render(
+			<MediaItem src="/image.png" title="" credit="Credit" caption="caption" />
+		);
 		expect(container.querySelector(".c-media-item__title")).toBeNull();
 	});
 	it("should not render credit if no caption", () => {
-		const { container } = render(<MediaItem title="title" credit="credit" caption="" />);
+		const { container } = render(
+			<MediaItem src="/image.png" title="title" credit="credit" caption="" />
+		);
 		expect(container.querySelector(".c-media-item__caption")).toBeNull();
 	});
 	it("should not render caption if empty object credit causing falsy string", () => {
-		const { container } = render(<MediaItem title="title" credit="" caption="caption" />);
+		const { container } = render(
+			<MediaItem src="/image.png" title="title" credit="" caption="caption" />
+		);
 		expect(container.querySelector(".c-media-item__credit")).toBeNull();
 	});
 	it("should take in html for the title prop", () => {
-		render(<MediaItem title="<p>title something special</p>" credit="Credit" caption="caption" />);
+		render(
+			<MediaItem
+				src="/image.png"
+				title="<p>title something special</p>"
+				credit="Credit"
+				caption="caption"
+			/>
+		);
 		expect(screen.queryByText("title something special")).not.toBeNull();
 	});
 	it("should take in html for the caption prop", () => {
 		render(
 			<MediaItem
+				src="/image.png"
 				title="title"
 				credit="Credit"
 				caption="<dialog data-testid='html-example-as-props'>caption</dialog>"
@@ -56,6 +76,7 @@ describe("MediaItem", () => {
 	it("should take in html for the credit prop", () => {
 		render(
 			<MediaItem
+				src="/image.png"
 				title="title"
 				credit="<dialog data-testid='html-example-as-props'>Credit</dialog>"
 				caption="caption"
@@ -64,5 +85,16 @@ describe("MediaItem", () => {
 		expect(screen.queryByText("Credit")).not.toBeNull();
 		const exampleHtmlCreditProp = screen.getByTestId("html-example-as-props");
 		expect(exampleHtmlCreditProp).not.toBeNull();
+	});
+	it("should render image", () => {
+		render(<MediaItem src="test-image.jpg" />);
+		const element = screen.getByRole("img");
+		expect(element).toHaveAttribute("src", "test-image.jpg");
+		expect(element).toHaveAttribute("alt", "");
+	});
+	it("should not render image but other fields there", () => {
+		render(<MediaItem src="" title="This is a title" />);
+		// returns null if no elements match
+		expect(screen.queryByRole("img")).toBeNull();
 	});
 });
