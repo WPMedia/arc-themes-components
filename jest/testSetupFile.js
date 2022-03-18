505e1d7232b0eae250ec24891b76173112f9d466
Eric Carlisle <eric@ericcarlisle.com>
2022-03-18 11:52:05 -0400

    Mock for matchMedia object.

--- jest/testSetupFile.js
+++ jest/testSetupFile.js
@@ -1,15 +1,19 @@
 import "@testing-library/jest-dom";
 
-window.matchMedia =
-	window.matchMedia ||
-	// eslint-disable-next-line func-names
-	function () {
-		return {
-			matches: false,
-			addListener() {},
-			removeListener() {},
-		};
-	};
+// eslint-disable-next-line no-undef
+Object.defineProperty(globalThis, "matchMedia", {
+	writable: true,
+	value: jest.fn().mockImplementation((query) => ({
+		matches: false,
+		media: query,
+		onchange: null,
+		addListener: jest.fn(), // Deprecated
+		removeListener: jest.fn(), // Deprecated
+		addEventListener: jest.fn(),
+		removeEventListener: jest.fn(),
+		dispatchEvent: jest.fn(),
+	})),
+});
 
 beforeEach(() => {
 	jest.resetModules();
