c6a2aaceae6ac52d5952775a3dec7df2a9663a89
Jack Howard <jackhowa@fastmail.com>
2022-03-21 09:45:21 -0500

    feat: Take in children within the figure element rather than the figcaption

--- src/components/media-item/index.jsx
+++ src/components/media-item/index.jsx
@@ -4,29 +4,31 @@ const COMPONENT_CLASS_NAME = "c-media-item";
 
 const MediaItem = ({ caption, children, className, credit, title }) =>
 	title || caption || credit ? (
-		<figcaption
+		<figure
 			className={className ? `${COMPONENT_CLASS_NAME} ${className}` : `${COMPONENT_CLASS_NAME}`}
 		>
 			{children}
-			{title ? (
-				<span
-					dangerouslySetInnerHTML={{ __html: `${title} ` }}
-					className={`${COMPONENT_CLASS_NAME}__title`}
-				/>
-			) : null}
-			{caption ? (
-				<span
-					dangerouslySetInnerHTML={{ __html: `${caption} ` }}
-					className={`${COMPONENT_CLASS_NAME}__caption`}
-				/>
-			) : null}
-			{credit ? (
-				<span
-					dangerouslySetInnerHTML={{ __html: `${credit} ` }}
-					className={`${COMPONENT_CLASS_NAME}__credit`}
-				/>
-			) : null}
-		</figcaption>
+			<figcaption className={`${COMPONENT_CLASS_NAME}__fig-caption`}>
+				{title ? (
+					<span
+						dangerouslySetInnerHTML={{ __html: `${title} ` }}
+						className={`${COMPONENT_CLASS_NAME}__title`}
+					/>
+				) : null}
+				{caption ? (
+					<span
+						dangerouslySetInnerHTML={{ __html: `${caption} ` }}
+						className={`${COMPONENT_CLASS_NAME}__caption`}
+					/>
+				) : null}
+				{credit ? (
+					<span
+						dangerouslySetInnerHTML={{ __html: `${credit} ` }}
+						className={`${COMPONENT_CLASS_NAME}__credit`}
+					/>
+				) : null}
+			</figcaption>
+		</figure>
 	) : null;
 
 MediaItem.propTypes = {
