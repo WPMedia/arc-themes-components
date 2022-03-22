ca6e45dba8edf3ca8c05ac722a016d611d140c99
Brent Miller <brent.miller@washport.com>
2022-03-22 15:29:59 -0500

    A11y audit and custom button update

--- src/components/carousel/index.jsx
+++ src/components/carousel/index.jsx
@@ -3,11 +3,14 @@ import Slider from "react-slick";
 
 const COMPONENT_CLASS_NAME = "c-carousel";
 
-const Carousel = ({ children, className, sliderProps }) => (
-	<div className={className ? `${COMPONENT_CLASS_NAME} ${className}` : `${COMPONENT_CLASS_NAME}`}>
-		<Slider {...sliderProps}>{children}</Slider>
-	</div>
-);
+const Carousel = ({ children, className, sliderProps }) => {
+	console.log("Here are the slider props..", sliderProps);
+	return (
+		<div className={className ? `${COMPONENT_CLASS_NAME} ${className}` : `${COMPONENT_CLASS_NAME}`}>
+			<Slider {...sliderProps}>{children}</Slider>
+		</div>
+	);
+};
 
 Carousel.propTypes = {
 	/** The text, images or any node that will be displayed within the component. */
