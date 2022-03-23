abf55cfc2d8436abc4b38d430ec1320f66cc502e
Brent Miller <brent.miller@washport.com>
2022-03-23 16:38:52 -0500

    a11y enhancements

--- src/components/carousel/index.jsx
+++ src/components/carousel/index.jsx
@@ -1,14 +1,63 @@
 import PropTypes from "prop-types";
 import Slider from "react-slick";
+import { useEffect, useRef } from "react";
 
 const COMPONENT_CLASS_NAME = "c-carousel";
 
-const Carousel = ({ children, className, sliderProps }) => (
-	<div className={className ? `${COMPONENT_CLASS_NAME} ${className}` : `${COMPONENT_CLASS_NAME}`}>
-		<Slider {...sliderProps}>{children}</Slider>
-	</div>
-);
+const Carousel = ({ children, className, sliderProps }) => {
+	const sliderRef = useRef();
+	const { onReInit: sliderReInit } = sliderProps;
 
+	/**
+	 * This useEffect will enhance a11y:
+	 * 1) It will add role and aria-label to the carousel component
+	 * 2) It will add needed roles and labels to each slide.
+	 * 3) If paging is being used it will set the current pager container to aria-current=true
+	 */
+	useEffect(() => {
+		const slider = sliderRef.current.querySelector(".slick-slider");
+		slider?.setAttribute("role", "region");
+		slider?.setAttribute("aria-label", "carousel");
+
+		const slides = slider?.querySelectorAll(".slick-slide");
+		slides?.forEach((slide) => {
+			slide.setAttribute("role", "group");
+			slide.setAttribute("aria-label", `slide ${parseInt(slide.dataset.index, 10) + 1}`);
+		});
+
+		const pager = slider?.querySelector(".slick-dots .slick-active > *");
+		if (pager) pager.setAttribute("aria-current", "true");
+	}, []);
+
+	/**
+	 * The onReInit is a callback of the react-slider.
+	 * It is being used to update the aria-current attribute on the pager if its being used.
+	 * It also checked to see if the caller passed in their own onReInit and if so executes it.
+	 */
+	const onReInit = () => {
+		const currentActive = sliderRef.current.querySelector(
+			'.slick-slider .slick-dots [aria-current="true"]'
+		);
+		const pager = sliderRef.current.querySelector(".slick-slider .slick-dots .slick-active > *");
+		if (currentActive && pager) {
+			currentActive.removeAttribute("aria-current");
+			pager.setAttribute("aria-current", "true");
+		}
+
+		if (sliderReInit) sliderReInit();
+	};
+
+	return (
+		<div
+			ref={sliderRef}
+			className={className ? `${COMPONENT_CLASS_NAME} ${className}` : `${COMPONENT_CLASS_NAME}`}
+		>
+			<Slider onReInit={onReInit} {...sliderProps}>
+				{children}
+			</Slider>
+		</div>
+	);
+};
 Carousel.propTypes = {
 	/** The text, images or any node that will be displayed within the component. */
 	children: PropTypes.node.isRequired,
--- src/components/carousel/index.test.jsx
+++ src/components/carousel/index.test.jsx
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
 import "@testing-library/jest-dom";
 
 import Carousel from ".";
@@ -149,5 +149,119 @@ describe("Carousel", () => {
 			</Carousel>
 		);
 		expect(container.querySelectorAll(".slick-arrow")).toHaveLength(0);
+	});
+	it("should ensure custom onReInit is called.", () => {
+		const { container } = render(
+			<Carousel
+				className="testOnReInit"
+				sliderProps={{
+					arrows: false,
+					centerMode: false,
+					infinite: false,
+					centerPadding: "60px",
+					slidesToShow: 4,
+					speed: 500,
+					slidesToScroll: 1,
+					initialSlide: 3,
+					onReInit: () => {
+						const parent = document.querySelector(".testOnReInit");
+						const div = document.createElement("div");
+						div.classList.add("injectedDiv");
+						parent.appendChild(div);
+					},
+				}}
+			>
+				<div>Card One</div>
+				<div>Card Two</div>
+				<div>Card Three</div>
+				<div>Card Four</div>
+				<div>Card Five</div>
+				<div>Card Six</div>
+				<div>Card Seven</div>
+				<div>Card Eight</div>
+			</Carousel>
+		);
+		expect(container.querySelectorAll(".injectedDiv")).toHaveLength(1);
+	});
+	it("should ensure custom onReInit is called when pager is used.", () => {
+		const { container } = render(
+			<Carousel
+				className="testOnReInit"
+				sliderProps={{
+					dots: true,
+					arrows: false,
+					centerMode: false,
+					infinite: false,
+					centerPadding: "60px",
+					slidesToShow: 4,
+					speed: 500,
+					slidesToScroll: 1,
+					initialSlide: 3,
+					onReInit: () => {
+						const parent = document.querySelector(".testOnReInit");
+						const div = document.createElement("div");
+						div.classList.add("injectedDiv");
+						parent.appendChild(div);
+					},
+				}}
+			>
+				<div>Card One</div>
+				<div>Card Two</div>
+				<div>Card Three</div>
+				<div>Card Four</div>
+				<div>Card Five</div>
+				<div>Card Six</div>
+				<div>Card Seven</div>
+				<div>Card Eight</div>
+			</Carousel>
+		);
+		expect(container.querySelectorAll(".injectedDiv")).toHaveLength(1);
+	});
+
+	it("should establish and update correct roles and Aria attributes", () => {
+		const { container } = render(
+			<Carousel
+				className="testOnReInit"
+				sliderProps={{
+					dots: true,
+					arrows: true,
+					centerMode: false,
+					infinite: false,
+					centerPadding: "60px",
+					slidesToShow: 4,
+					speed: 500,
+					slidesToScroll: 1,
+					initialSlide: 0,
+				}}
+			>
+				<div>Card One</div>
+				<div>Card Two</div>
+				<div>Card Three</div>
+				<div>Card Four</div>
+				<div>Card Five</div>
+				<div>Card Six</div>
+				<div>Card Seven</div>
+				<div>Card Eight</div>
+			</Carousel>
+		);
+
+		expect(container.querySelector(".slick-slider")).toHaveAttribute("aria-label", "carousel");
+
+		expect(container.querySelector(".slick-slider")).toHaveAttribute("role", "region");
+
+		expect(container.querySelector(".slick-current")).toHaveAttribute("aria-label", "slide 1");
+
+		expect(
+			container.querySelector('.slick-slider .slick-dots [aria-current="true"]')
+		).toContainHTML('<button aria-current="true">1</button>');
+
+		const button = container.querySelector(".slick-next");
+		fireEvent.click(button);
+
+		expect(container.querySelector(".slick-current")).toHaveAttribute("aria-label", "slide 2");
+
+		expect(
+			container.querySelector('.slick-slider .slick-dots [aria-current="true"]')
+		).toContainHTML('<button aria-current="true">2</button>');
 	});
 });
