55f2edc005ba1733c5f2711eaf1e48814aa88ab0
Jack Howard <jackhowa@fastmail.com>
2022-03-25 14:18:54 -0500

    feat: Add basic video player that requires a reload for now

--- src/components/video/index.jsx
+++ src/components/video/index.jsx
@@ -1,10 +1,33 @@
 import PropTypes from "prop-types";
+import EmbedContainer from "react-oembed-container";
 
 const COMPONENT_CLASS_NAME = "c-video";
 
-const Video = ({ className, aspectRatio, viewportPercentage }) => {
+// simple util function
+function convertStringToNode(string) {
+	const parser = new DOMParser();
+	const doc = parser.parseFromString(string, "text/html");
+	// get the body, will return <body> around your code
+	return doc.body;
+}
+
+// taking in embed code
+// adding on any powa fields
+// todo: return the embed code with the correct statuses
+const getEmbedHTMLWithPlayStatus = (embedMarkup) => {
+	if (embedMarkup) {
+		const embedHTMLWithPlayStatus = convertStringToNode(embedMarkup);
+
+		// innerhtml ensures body tag not rendered
+		return embedHTMLWithPlayStatus.innerHTML;
+	}
+	return "";
+};
+
+const Video = ({ className, aspectRatio, viewportPercentage, embedMarkup }) => {
 	const containerClassNames = [COMPONENT_CLASS_NAME, className].filter((i) => i).join(" ");
 
+	// todo: implement client-side only check
 	return (
 		<div
 			className={containerClassNames}
@@ -12,7 +35,11 @@ const Video = ({ className, aspectRatio, viewportPercentage }) => {
 				"--aspect-ratio": aspectRatio,
 				"--viewport-percentage": viewportPercentage,
 			}}
-		/>
+		>
+			<EmbedContainer markup={getEmbedHTMLWithPlayStatus(embedMarkup)}>
+				<div dangerouslySetInnerHTML={{ __html: getEmbedHTMLWithPlayStatus(embedMarkup) }} />
+			</EmbedContainer>
+		</div>
 	);
 };
 
