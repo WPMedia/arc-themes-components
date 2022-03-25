97bcda8ec98d5590c732c8c0805bed7a2fc651c5
Jack Howard <jackhowa@fastmail.com>
2022-03-25 13:55:16 -0500

    refactor: Use aspect-ratio rather than padding-bottom per comment
    via https://web.dev/aspect-ratio/#maintaining-aspect-ratio-with-aspect-ratio

--- src/components/video/index.jsx
+++ src/components/video/index.jsx
@@ -3,15 +3,7 @@ import PropTypes from "prop-types";
 const COMPONENT_CLASS_NAME = "c-video";
 
 const Video = ({ className, aspectRatio, viewportPercentage }) => {
-	const videoIsLoading = true;
-
-	const containerClassNames = [
-		COMPONENT_CLASS_NAME,
-		videoIsLoading && `${COMPONENT_CLASS_NAME}--not-loaded`,
-		className,
-	]
-		.filter((i) => i)
-		.join(" ");
+	const containerClassNames = [COMPONENT_CLASS_NAME, className].filter((i) => i).join(" ");
 
 	return (
 		<div
