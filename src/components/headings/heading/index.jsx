e49f9278cb508c70445b762f271883c183dd1a6b
Matthew Roach <matthewroach@users.noreply.github.com>
2022-04-21 16:58:18 +0100

    Heading - Add truncation (#77)

--- src/components/headings/heading/index.jsx
+++ src/components/headings/heading/index.jsx
@@ -3,7 +3,7 @@ import LevelContext from "../context";
 
 const COMPONENT_CLASS_NAME = "c-heading";
 
-const Heading = ({ className, children, ...rest }) => (
+const Heading = ({ className, children, truncationLines, ...rest }) => (
 	<LevelContext.Consumer>
 		{(level) => {
 			// max heading level is 6
@@ -12,6 +12,7 @@ const Heading = ({ className, children, ...rest }) => (
 			return (
 				<HeadingTag
 					{...rest}
+					style={{ "--heading-truncation": truncationLines > 0 ? truncationLines : null }}
 					className={className ? `${COMPONENT_CLASS_NAME} ${className}` : `${COMPONENT_CLASS_NAME}`}
 				>
 					{children}
@@ -26,6 +27,10 @@ Heading.propTypes = {
 	className: PropTypes.string,
 	/** The text, images or any node that will be displayed within the component */
 	children: PropTypes.node.isRequired,
+	/** Number of lines to show before being truncated and augmented with ellipses.
+	 	Default value is `0` and results in no truncation of content.
+	 */
+	truncationLines: PropTypes.number,
 };
 
 export default Heading;
