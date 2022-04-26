440ef76b7951965ef93330be430397b46b406e22
Matthew Roach <matthewroach@users.noreply.github.com>
2022-04-26 09:32:22 +0100

    Update inconsistency of file naming to follow existing pattern of index (#83)

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ import Stack from "./src/components/stack";
 import Video from "./src/components/video";
 import formatCredits from "./src/utils/format-credits";
 import formatPowaVideoEmbed from "./src/utils/format-powa-video-embed";
-import isServerSide from "./src/utils/is-server-side/isServerSide";
-import useInterval from "./src/utils/use-interval/useInterval";
+import useInterval from "./src/utils/hooks/use-interval";
+import isServerSide from "./src/utils/is-server-side";
 
 export {
 	Attribution,
