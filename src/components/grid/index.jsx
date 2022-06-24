166a4f2e15d3193e875a9011ab642d4b2273c1cb
Matthew Roach <matthewroach@users.noreply.github.com>
2022-06-24 15:36:57 +0100

    Update Grid to have an as prop for denoting the HTML element to use (#107)

--- src/components/grid/index.jsx
+++ src/components/grid/index.jsx
@@ -2,16 +2,25 @@ import PropTypes from "prop-types";
 
 const COMPONENT_CLASS_NAME = "c-grid";
 
-const Grid = ({ children, className, ...rest }) => (
-	<div
-		{...rest}
-		className={className ? `${COMPONENT_CLASS_NAME} ${className}` : `${COMPONENT_CLASS_NAME}`}
-	>
-		{children}
-	</div>
-);
+const Grid = ({ as, children, className, ...rest }) => {
+	const Element = `${as}`;
+	return (
+		<Element
+			{...rest}
+			className={className ? `${COMPONENT_CLASS_NAME} ${className}` : `${COMPONENT_CLASS_NAME}`}
+		>
+			{children}
+		</Element>
+	);
+};
+
+Grid.defaultProps = {
+	as: "div",
+};
 
 Grid.propTypes = {
+	/** Specify the HTML Element to use to render as */
+	as: PropTypes.string,
 	/** Class name(s) that get appended to default class name of the component */
 	className: PropTypes.string,
 	/** The text, images or any node that will be displayed within the component */
