dcceb8e9aa330fec98abeaaa4ea09d5baa8de4a0
Jack Howard <jackhowa@fastmail.com>
2022-07-08 11:37:09 +0100

    feat: Take in resizer options of height and width
    Remove width and height fields from existing Image component in the arc themes components repo. The width and height properties of the img tag are determined by what’s inside the resizerOptions

--- src/components/image/index.jsx
+++ src/components/image/index.jsx
@@ -2,20 +2,24 @@ import PropTypes from "prop-types";
 
 const COMPONENT_CLASS_NAME = "c-image";
 
-const Image = ({ alt, className, height, loading, src, width }) => (
-	<img
-		alt={alt}
-		className={className ? `${COMPONENT_CLASS_NAME} ${className}` : `${COMPONENT_CLASS_NAME}`}
-		height={height}
-		loading={loading}
-		src={src}
-		width={width}
-	/>
-);
+const Image = ({ alt, className, loading, src, resizedOptions }) => {
+	const { width, height } = resizedOptions;
+	return (
+		<img
+			alt={alt}
+			className={className ? `${COMPONENT_CLASS_NAME} ${className}` : `${COMPONENT_CLASS_NAME}`}
+			height={height}
+			loading={loading}
+			src={src}
+			width={width}
+		/>
+	);
+};
 
 Image.defaultProps = {
 	alt: "",
 	loading: "lazy",
+	resizedOptions: {},
 };
 
 Image.propTypes = {
@@ -23,14 +27,16 @@ Image.propTypes = {
 	alt: PropTypes.string,
 	/** Class name(s) that get appended to default class name of the component */
 	className: PropTypes.string,
-	/** The intrinsic height of the image in pixels */
-	height: PropTypes.number,
+	resizedOptions: PropTypes.shape({
+		/** The intrinsic width of the image in pixels */
+		width: PropTypes.number,
+		/** The intrinsic height of the image in pixels */
+		height: PropTypes.number,
+	}),
 	/** Indication of how the browser should load the image, using the native loading attribute of an <img /> tag */
 	loading: PropTypes.oneOf(["lazy", "eager"]),
 	/** The URL to an image to load and display */
 	src: PropTypes.string.isRequired,
-	/** The intrinsic width of the image in pixels */
-	width: PropTypes.number,
 };
 
 export default Image;
--- src/components/image/index.test.jsx
+++ src/components/image/index.test.jsx
@@ -19,4 +19,25 @@ describe("Image", () => {
 		expect(element).toHaveClass(ADDITIONAL_CLASSES);
 		expect(element).toHaveClass(ORIGINAL_CLASSES);
 	});
+
+	it("should render no height and no width if no height and width", () => {
+		render(<Image src="test-image.jpg" />);
+		const element = screen.getByRole("img");
+		expect(element).not.toHaveAttribute("height");
+		expect(element).not.toHaveAttribute("width");
+	});
+
+	it("should render height and width if height and width", () => {
+		render(<Image src="test-image.jpg" resizedOptions={{ width: 100, height: 100 }} />);
+		const element = screen.getByRole("img");
+		expect(element).toHaveAttribute("height", "100");
+		expect(element).toHaveAttribute("width", "100");
+	});
+
+	it("should render only height if height and no width", () => {
+		render(<Image src="test-image.jpg" resizedOptions={{ height: 100 }} />);
+		const element = screen.getByRole("img");
+		expect(element).toHaveAttribute("height", "100");
+		expect(element).not.toHaveAttribute("width");
+	});
 });
