9db6eb1d4b1d74d4271820b80dfe0c7b64ae5bc3
Matthew Roach <matthewroach@users.noreply.github.com>
2022-07-11 18:57:07 +0100

    Update test

--- src/components/details/index.test.jsx
+++ src/components/details/index.test.jsx
@@ -24,7 +24,7 @@ describe("Details", () => {
 
 	it("should render with custom Icon", () => {
 		render(
-			<Details summary="Summary" icon={<>Icon</>}>
+			<Details summary="Summary" icon={<>Icon</>} iconPlacement="left">
 				Hello World
 			</Details>
 		);
