40021da717b34719ee525b69f69314bf73a162c2
Jack Howard <jackhowa@fastmail.com>
2022-07-12 16:45:58 +0100

    feat: Add camera icon resized

--- src/components/icon/icons/index.js
+++ src/components/icon/icons/index.js
@@ -1,5 +1,6 @@
 export { default as ArrowLeft } from "./ArrowLeft";
 export { default as ArrowRight } from "./ArrowRight";
+export { default as Camera } from "./Camera";
 export { default as ChevronDown } from "./ChevronDown";
 export { default as ChevronLeft } from "./ChevronLeft";
 export { default as ChevronRight } from "./ChevronRight";
