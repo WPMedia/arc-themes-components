00276e5254116effa496dbcda90237792fb45d58
Eric Carlisle <eric@ericcarlisle.com>
2022-08-03 08:03:18 -0400

    Correct  to  on  util.

--- src/utils/format-image-resizer-src/index.js
+++ src/utils/format-image-resizer-src/index.js
@@ -1,10 +1,10 @@
-const formatSrc = (src, resizerOptions, width, height) =>
-	// Use resizerOptions as querystring params on the src URL. If width and/or height are provided
-	// both in resizerOptions as well as function arguments, the function arguments will be used.
+const formatSrc = (src, resizedOptions, width, height) =>
+	// Use resizedOptions as querystring params on the src URL. If width and/or height are provided
+	// both in resizedOptions as well as function arguments, the function arguments will be used.
 	src.concat(
 		"?",
 		new URLSearchParams({
-			...resizerOptions,
+			...resizedOptions,
 			...(width ? { width } : {}),
 			...(height ? { height } : {}),
 		}).toString()
