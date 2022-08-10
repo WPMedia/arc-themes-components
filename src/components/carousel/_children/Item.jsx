993b96fea4ab027398e7370aa97b5e5ab73414f0
Jack Howard <jackhowa@fastmail.com>
2022-08-10 11:23:30 -0500

    feat: Pass in classname to item for further customization

--- src/components/carousel/_children/Item.jsx
+++ src/components/carousel/_children/Item.jsx
@@ -2,23 +2,23 @@
 import PropTypes from "prop-types";
 import { Children, cloneElement } from "react";
 
-const Item = ({ children, label, viewable, ...rest }) => (
+const Item = ({ children, className, label, viewable, ...rest }) => (
 	<div
 		{...rest}
 		role="group"
 		aria-roledescription="slide"
 		tabIndex={viewable ? null : "-1"}
-		className="c-carousel__slide"
+		className={["c-carousel__slide", className].filter((i) => i).join(" ")}
 		aria-label={label}
 		aria-hidden={viewable ? null : true}
 	>
 		{typeof children === "function"
 			? children({ viewable })
 			: Children.map(children, (child) =>
-				cloneElement(child, {
-					"aria-hidden": viewable ? null : true,
-					tabIndex: !viewable ? "-1" : null,
-				})
+					cloneElement(child, {
+						"aria-hidden": viewable ? null : true,
+						tabIndex: !viewable ? "-1" : null,
+					})
 			  )}
 	</div>
 );
