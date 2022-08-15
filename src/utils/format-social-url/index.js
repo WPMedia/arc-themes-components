81b87c54ab0afa5a93333329550981cc58c56e35
Nick Schubach <nschubach@gmail.com>
2022-08-15 14:26:01 -0400

    some updates and documentation

--- src/utils/format-social-url/index.js
+++ src/utils/format-social-url/index.js
@@ -1,56 +1,52 @@
-const SocialTypes = {
-	Email: "email",
-	Twitter: "twitter",
-	Instagram: "instagram",
-	Snapchat: "snapchat",
-	LinkedIn: "linkedin",
-	Reddit: "reddit",
-	Youtube: "youtube",
-	Medium: "medium",
-	Tumblr: "tumblr",
-	Pinterest: "pinterest",
-	SoundCloud: "soundcloud",
-	WhatsApp: "whatsapp",
-};
+const SocialTypes = Object.freeze({
+	Email: Symbol("email"),
+	Facebook: Symbol("facebook"),
+	Instagram: Symbol("instagram"),
+	LinkedIn: Symbol("linkedin"),
+	Medium: Symbol("medium"),
+	Pinterest: Symbol("pinterest"),
+	Reddit: Symbol("reddit"),
+	Snapchat: Symbol("snapchat"),
+	SoundCloud: Symbol("soundcloud"),
+	Tumblr: Symbol("tumblr"),
+	Twitter: Symbol("twitter"),
+	WhatsApp: Symbol("whatsapp"),
+	Youtube: Symbol("youtube"),
+});
 
 function formatSocialURL(type, field) {
 	switch (type) {
 		case SocialTypes.Email:
 			return `mailto:${field}`;
-		case SocialTypes.Twitter:
-			// https://twitter.com/jack
-			return `https://twitter.com/${field}`;
+		case SocialTypes.Facebook:
+			return `https://www.thefacebook.com/${field}`;
 		case SocialTypes.Instagram:
-			// https://www.instagram.com/zuck/
 			return `https://www.instagram.com/${field}/`;
-		case SocialTypes.Snapchat:
-			// https://www.snapchat.com/add/slc56
-			return `https://www.snapchat.com/add/${field}`;
 		case SocialTypes.LinkedIn:
-			// https://www.linkedin.com/in/jackhowa/
 			return `https://www.linkedin.com/in/${field}/`;
-		case SocialTypes.Reddit:
-			// https://www.reddit.com/user/NotAnishKapoor/
-			return `https://www.reddit.com/user/${field}/`;
-		case SocialTypes.Youtube:
-			// https://www.youtube.com/user/chasereeves
-			return `https://www.youtube.com/user/${field}`;
 		case SocialTypes.Medium:
-			// weird requires @ signs
-			// https://medium.com/@dan_abramov
 			return `https://medium.com/${field}`;
-		case SocialTypes.Tumblr:
-			// john green https://fishingboatproceeds.tumblr.com
-			return `https://${field}.tumblr.com`;
 		case SocialTypes.Pinterest:
 			return `https://www.pinterest.com/${field}/`;
+		case SocialTypes.Reddit:
+			return `https://www.reddit.com/user/${field}/`;
+		case SocialTypes.Snapchat:
+			return `https://www.snapchat.com/add/${field}`;
 		case SocialTypes.SoundCloud:
 			return `https://soundcloud.com/${field}`;
+		case SocialTypes.Tumblr:
+			return `https://${field}.tumblr.com`;
+		case SocialTypes.Twitter:
+			return `https://twitter.com/${field}`;
 		case SocialTypes.WhatsApp:
 			return `https://wa.me/${field}`;
+		case SocialTypes.Youtube:
+			return `https://www.youtube.com/user/${field}`;
 		default:
 			return field;
 	}
 }
+
+formatSocialURL.types = SocialTypes;
 
 export default formatSocialURL;
--- src/utils/format-social-url/index.test.js
+++ src/utils/format-social-url/index.test.js
@@ -2,51 +2,69 @@ import formatSocialURL from ".";
 
 describe("construct social url helper", () => {
 	it("takes in email as a field and returns interpolated string", () => {
-		expect(formatSocialURL("email", "username")).toBe("mailto:username");
-	});
-	it("takes in twitter as a field and returns interpolated string", () => {
-		expect(formatSocialURL("twitter", "username")).toBe("https://twitter.com/username");
-	});
-	it("takes in instagram as a field and returns interpolated string", () => {
-		expect(formatSocialURL("instagram", "username")).toBe("https://www.instagram.com/username/");
+		expect(formatSocialURL(formatSocialURL.types.Email, "username")).toBe("mailto:username");
 	});
 	it("takes in facebook as a field and returns interpolated string", () => {
-		expect(formatSocialURL("facebook", "https://www.thefacebook.com/zuck")).toBe(
-			"https://www.thefacebook.com/zuck"
+		expect(formatSocialURL(formatSocialURL.types.Facebook, "username")).toBe(
+			"https://www.thefacebook.com/username"
 		);
 	});
-	it("takes in reddit as a field and returns interpolated string", () => {
-		expect(formatSocialURL("reddit", "username")).toBe("https://www.reddit.com/user/username/");
+	it("takes in instagram as a field and returns interpolated string", () => {
+		expect(formatSocialURL(formatSocialURL.types.Instagram, "username")).toBe(
+			"https://www.instagram.com/username/"
+		);
 	});
-	it("takes in youtube as a field and returns interpolated string", () => {
-		expect(formatSocialURL("youtube", "chasereeves")).toBe(
-			"https://www.youtube.com/user/chasereeves"
+	it("takes in linkedin as a field and returns interpolated string", () => {
+		expect(formatSocialURL(formatSocialURL.types.LinkedIn, "username")).toBe(
+			"https://www.linkedin.com/in/username/"
 		);
 	});
 	it("takes in medium as a field and returns interpolated string", () => {
-		expect(formatSocialURL("medium", "@username")).toBe("https://medium.com/@username");
-	});
-	it("takes in tumblr as a field and returns interpolated string", () => {
-		expect(formatSocialURL("tumblr", "fishingboatproceeds")).toBe(
-			"https://fishingboatproceeds.tumblr.com"
+		expect(formatSocialURL(formatSocialURL.types.Medium, "@username")).toBe(
+			"https://medium.com/@username"
 		);
 	});
 	it("takes in pinterest as a field and returns interpolated string", () => {
-		expect(formatSocialURL("pinterest", "username")).toBe("https://www.pinterest.com/username/");
+		expect(formatSocialURL(formatSocialURL.types.Pinterest, "username")).toBe(
+			"https://www.pinterest.com/username/"
+		);
+	});
+	it("takes in reddit as a field and returns interpolated string", () => {
+		expect(formatSocialURL(formatSocialURL.types.Reddit, "username")).toBe(
+			"https://www.reddit.com/user/username/"
+		);
 	});
 	it("takes in snapchat as a field and returns interpolated string", () => {
-		expect(formatSocialURL("snapchat", "username")).toBe("https://www.snapchat.com/add/username");
-	});
-	it("takes in whatsapp as a field and returns interpolated string", () => {
-		expect(formatSocialURL("whatsapp", "phonenum")).toBe("https://wa.me/phonenum");
-	});
-	it("takes in linkedin as a field and returns interpolated string", () => {
-		expect(formatSocialURL("linkedin", "username")).toBe("https://www.linkedin.com/in/username/");
-	});
-	it("takes in rss as a field and returns interpolated string", () => {
-		expect(formatSocialURL("rss", "www.blog-rss.com")).toBe("www.blog-rss.com");
+		expect(formatSocialURL(formatSocialURL.types.Snapchat, "username")).toBe(
+			"https://www.snapchat.com/add/username"
+		);
 	});
 	it("takes in soundcloud as a field and returns interpolated string", () => {
-		expect(formatSocialURL("soundcloud", "username")).toBe("https://soundcloud.com/username");
+		expect(formatSocialURL(formatSocialURL.types.SoundCloud, "username")).toBe(
+			"https://soundcloud.com/username"
+		);
+	});
+	it("takes in twitter as a field and returns interpolated string", () => {
+		expect(formatSocialURL(formatSocialURL.types.Twitter, "username")).toBe(
+			"https://twitter.com/username"
+		);
+	});
+	it("takes in tumblr as a field and returns interpolated string", () => {
+		expect(formatSocialURL(formatSocialURL.types.Tumblr, "username")).toBe(
+			"https://username.tumblr.com"
+		);
+	});
+	it("takes in youtube as a field and returns interpolated string", () => {
+		expect(formatSocialURL(formatSocialURL.types.Youtube, "username")).toBe(
+			"https://www.youtube.com/user/username"
+		);
+	});
+	it("takes in whatsapp as a field and returns interpolated string", () => {
+		expect(formatSocialURL(formatSocialURL.types.WhatsApp, "phonenum")).toBe(
+			"https://wa.me/phonenum"
+		);
+	});
+	it("returns the field value if unknown", () => {
+		expect(formatSocialURL("unknown value", "username")).toBe("username");
 	});
 });
