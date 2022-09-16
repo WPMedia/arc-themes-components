3c44942f794dbe9cf9d0eedaffb387b9dc45a770
nschubach <nschubach@gmail.com>
2022-09-16 16:03:11 -0400

    Add sign-images-in-ans-object (#143)

---
+++ src/utils/sign-images-in-ans-object/index.test.js
@@ -0,0 +1,107 @@
+import signImagesInANSObject from ".";
+
+const data = {
+	_id: "43UU6MCQERAMRPTD23B3CEXE7E",
+	type: "story",
+	content_elements: [
+		{
+			_id: "LJJSIEXMZ5FTDBP7PFHXI5A4XY",
+			type: "image", // should fill auth
+		},
+		{
+			_id: "LJJSIEXMZ5FTDBP7PFHXI5A4XY",
+			type: "image", // should fill with first items auth
+		},
+		{
+			_id: "LJJSIEXMZ5FTDBP7PFHXI5A4XZ",
+			type: "image",
+			auth: {
+				1: "40b3b900866998ec98c4a286eef727080a10ac968d5eed7bd4a6a084511db6c1",
+			}, // should get 2
+		},
+		{
+			_id: "LJJSIEXMZ5FTDBP7PFHXI5A4X2",
+			type: "image",
+			auth: {
+				2: "40b3b900866998ec98c4a286eef727080a10ac968d5eed7bd4a6a084511db6c2",
+			}, // should skip
+		},
+	],
+	promo_items: {
+		basic: {
+			_id: "OYRQQIJJLNBVNN4QLERLG2FZZ4",
+			type: "image", // should fill auth
+		},
+	},
+};
+
+const idAuthMap = {
+	LJJSIEXMZ5FTDBP7PFHXI5A4XY: "40b3b900866998ec98c4a286eef727080a10ac968d5eed7bd4a6a084511db6cy",
+	LJJSIEXMZ5FTDBP7PFHXI5A4XZ: "40b3b900866998ec98c4a286eef727080a10ac968d5eed7bd4a6a084511db6cz",
+	LJJSIEXMZ5FTDBP7PFHXI5A4X2: "40b3b900866998ec98c4a286eef727080a10ac968d5eed7bd4a6a084511db6c2",
+	OYRQQIJJLNBVNN4QLERLG2FZZ4: "545c018dbf2bbc8e4488c7546167e6afacc259cf4fe0b2f28c8043990f689e40",
+};
+
+const fetcher = jest.fn((id) => idAuthMap[id]);
+const cachedCall = jest.fn((cacheId, fetchMethod, options) =>
+	Promise.resolve(fetchMethod(options.query.id))
+);
+
+describe("Sign Images In ANS Object", () => {
+	it("returns the correct auth key values in the returned ans object", async () => {
+		const signIt = signImagesInANSObject(cachedCall, fetcher, 2);
+
+		const { data: signedData, status } = await signIt({ data, status: 2 });
+
+		expect(status).toBe(2);
+		expect(cachedCall).toHaveBeenCalledWith(
+			"image-token-LJJSIEXMZ5FTDBP7PFHXI5A4XY",
+			fetcher,
+			expect.objectContaining({
+				query: { id: "LJJSIEXMZ5FTDBP7PFHXI5A4XY" },
+				ttl: 31536000,
+				independent: true,
+			})
+		);
+		expect(cachedCall).toHaveBeenCalledWith(
+			"image-token-LJJSIEXMZ5FTDBP7PFHXI5A4XZ",
+			fetcher,
+			expect.objectContaining({
+				query: { id: "LJJSIEXMZ5FTDBP7PFHXI5A4XZ" },
+				ttl: 31536000,
+				independent: true,
+			})
+		);
+		expect(cachedCall).toHaveBeenCalledWith(
+			"image-token-OYRQQIJJLNBVNN4QLERLG2FZZ4",
+			fetcher,
+			expect.objectContaining({
+				query: { id: "OYRQQIJJLNBVNN4QLERLG2FZZ4" },
+				ttl: 31536000,
+				independent: true,
+			})
+		);
+		expect(cachedCall).not.toHaveBeenCalledWith(
+			"image-token-LJJSIEXMZ5FTDBP7PFHXI5A4X2",
+			fetcher,
+			expect.objectContaining({
+				query: { id: "LJJSIEXMZ5FTDBP7PFHXI5A4X2" },
+				ttl: 31536000,
+				independent: true,
+			})
+		);
+		expect(cachedCall).toHaveBeenCalledTimes(3);
+		expect(signedData.promo_items.basic.auth[2]).toBe(
+			"545c018dbf2bbc8e4488c7546167e6afacc259cf4fe0b2f28c8043990f689e40"
+		);
+		expect(signedData.content_elements[0].auth[2]).toBe(
+			"40b3b900866998ec98c4a286eef727080a10ac968d5eed7bd4a6a084511db6cy"
+		);
+		expect(signedData.content_elements[1].auth[2]).toBe(
+			"40b3b900866998ec98c4a286eef727080a10ac968d5eed7bd4a6a084511db6cy"
+		);
+		expect(signedData.content_elements[2].auth[2]).toBe(
+			"40b3b900866998ec98c4a286eef727080a10ac968d5eed7bd4a6a084511db6cz"
+		);
+	});
+});
