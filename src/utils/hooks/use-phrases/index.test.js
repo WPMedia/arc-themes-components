82d669b87ed7bd58708dbf32ab18607f55f41c87
Nick Schubach <nschubach@gmail.com>
2022-10-06 17:10:16 -0400

    Add tests

---
+++ src/utils/hooks/use-phrases/index.test.js
@@ -0,0 +1,16 @@
+import usePhrases from ".";
+import getProperties from "fusion:properties";
+
+jest.mock("fusion:properties");
+
+describe("usePhrases hook", () => {
+	it("should return phrases object", () => {
+		expect(usePhrases()).toHaveProperty("t");
+		expect(typeof usePhrases().t).toBe("function");
+	});
+	it("should default to 'en' if no locale property is found", () => {
+		getProperties.mockImplementationOnce(() => ({}));
+		expect(usePhrases()).toHaveProperty("t");
+		expect(typeof usePhrases().t).toBe("function");
+	});
+});
