f4ec5819b250fb75ab323fa11ebaf45a7bc48227
Nick Schubach <nschubach@gmail.com>
2022-10-10 16:16:56 -0400

    fix tests to be more accurate

--- src/utils/hooks/use-phrases/index.test.js
+++ src/utils/hooks/use-phrases/index.test.js
@@ -1,17 +1,20 @@
+import getTranslatedPhrases from "fusion:intl";
 import getProperties from "fusion:properties";
 
 import usePhrases from ".";
 
+jest.mock("fusion:intl");
 jest.mock("fusion:properties");
 
 describe("usePhrases hook", () => {
 	it("should return phrases object", () => {
-		expect(usePhrases()).toHaveProperty("t");
-		expect(typeof usePhrases().t).toBe("function");
+		const phrases = usePhrases();
+		expect(phrases).toHaveProperty("t");
+		expect(typeof phrases.t).toBe("function");
 	});
 	it("should default to 'en' if no locale property is found", () => {
 		getProperties.mockImplementationOnce(() => ({}));
 		expect(usePhrases()).toHaveProperty("t");
-		expect(typeof usePhrases().t).toBe("function");
+		expect(getTranslatedPhrases).toHaveBeenCalledWith("en");
 	});
 });
