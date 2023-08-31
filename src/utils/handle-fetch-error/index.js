18c63dcb43e18c1edc2955ec7f9604b1e4b17763
blakeganderson <85515364+blakeganderson@users.noreply.github.com>
2023-08-31 14:25:48 -0400

    updated handle fetch error

--- src/utils/handle-fetch-error/index.js
+++ src/utils/handle-fetch-error/index.js
@@ -1,19 +1,17 @@
 const handleFetchError = (error) => {
-	if (error?.response) {
-		if (error.response?.status === 404) {
-			const NotFoundError = (message = "Not Found") => {
-				const err = new Error();
-				err.message = message;
-				err.statusCode = 404;
-				return err;
-			};
-			throw NotFoundError();
-		}
+	if (error.response?.status === 404) {
+		const NotFoundError = (message = "Not Found") => {
+			const err = new Error(message);
+			err.statusCode = 404;
+			return err;
+		};
+		throw NotFoundError();
+	} else if (error?.statusCode === 302 || error.statusCode === 404) {
+		throw error;
+	} else if (error?.response) {
 		throw new Error(
 			`The response from the server was an error with the status code ${error?.response?.status}.`
 		);
-	} else if (error?.statusCode === 302) {
-		throw error;
 	} else if (error?.request) {
 		throw new Error("The request to the server failed with no response.");
 	} else {
