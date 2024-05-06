acfeba306f9f18e024074849539ed74769fc6c31
Laura Alejandra Pinilla Buitrago <laura.pinilla@wizeline.com>
2024-05-06 07:37:13 -0600

    fix reCaptcha

--- src/components/bot-challenge-protection/index.jsx
+++ src/components/bot-challenge-protection/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import ReCAPTCHA from "react-google-recaptcha";
 import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
 import PropTypes from "prop-types";
@@ -21,21 +21,25 @@ const BotChallengeProtection = ({
 }) => {
 	const { isInitialized } = useIdentity();
 	const { recaptchaVersion, siteKey, isRecaptchaEnabled } = useRecaptcha(challengeIn);
-	const [ currentRecapctha, setCurrentRecaptcha ] = useState();
+	//const [ currentRecapctha, setCurrentRecaptcha ] = useState();
 
-	const recaptchaRef = React.createRef();
+	const recaptchaRef = useRef();
+	console.log("reCaptchaRef")
+	console.log(recaptchaRef);
+	//const recaptchaRef = React.createRef();
 
-	useEffect(()=>{
-		const {current} = recaptchaRef;
-		setCurrentRecaptcha(current);
+	// useEffect(()=>{
+	// 	const {current} = recaptchaRef;
+	// 	setCurrentRecaptcha(current);
 
-	},[recaptchaRef]);
+	// },[recaptchaRef]);
 
 	useEffect(() => {
-		if (isRecaptchaEnabled && recaptchaVersion === RECAPTCHA_V2 && (captchaError || error) && currentRecapctha) {
-			currentRecapctha.reset();
+		if (isRecaptchaEnabled && recaptchaVersion === RECAPTCHA_V2 && (captchaError || error)) {
+			console.log("Entro....!!!")
+			recaptchaRef.current.reset();
 		}
-	}, [captchaError, error, isRecaptchaEnabled, recaptchaVersion, resetRecaptcha, currentRecapctha]);
+	}, [captchaError, error, isRecaptchaEnabled, recaptchaVersion, resetRecaptcha, recaptchaRef]);
 
 	const onChange = (value) => {
 		setCaptchaToken(value);
