4567a9ee831453b587a49af28e7909257be35729
nschubach <nschubach@gmail.com>
2024-05-14 14:05:16 -0400

    Update src/utils/sign-images-in-ans-object/index.js

--- src/utils/sign-images-in-ans-object/index.js
+++ src/utils/sign-images-in-ans-object/index.js
@@ -11,7 +11,7 @@ const transformAuthorImages = (value) => {
 		};
 	}
 
-	if (type === "author" && image?.match(/^https?://(-\.)?([^\s/?\.#-]+\.?)+(/[^\s]*)?$/i)) {
+	if (type === "author" && image?.match(/^https?:\/\/(-\.)?([^\s\/?\.#-]+\.?)+(\/[^\s]*)?$\/i)) {
 		return {
 			...value,
 			ansImage: {
