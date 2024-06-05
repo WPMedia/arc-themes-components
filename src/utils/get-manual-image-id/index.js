6a55f9d17b0d0ad04da81993fb741a1b6d2152aa
Vito Galatro <vjgalatro@gmail.com>
2024-06-05 13:26:21 -0400

    THEMES-1906: updating regex

--- src/utils/get-manual-image-id/index.js
+++ src/utils/get-manual-image-id/index.js
@@ -1,7 +1,8 @@
 const getManualImageID = (imageURL, isResizedImage = false) => {
 	let manualImageId = "";
 	if (!isResizedImage && imageURL) {
-		const cloudfrontRegex = /cloudfront.*images.arcpublishing.*\/([\w]{26})(?:$|\.[\w]{3,4})/;
+		const cloudfrontRegex =
+			/^https:\/\/cloudfront.*images\.arcpublishing.*\/(\w{26})(?:$|\.\w{3,4})/;
 		const matches = imageURL.match(cloudfrontRegex);
 		manualImageId = Array.isArray(matches) && matches.length >= 2 ? matches[1] : "";
 	}
