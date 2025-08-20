f71b2def8f17418eed2d2aa7374009f9146bf7c6
Anna <sherman.anna@gmail.com>
2025-08-20 16:22:13 -0400

    log

--- src/components/image/index.jsx
+++ src/components/image/index.jsx
@@ -27,6 +27,7 @@ const Image = ({
 	const formattedSrc = ansImage ? imageANSToImageSrc(ansImage) : src;
 	const { arcSite } = useFusionContext();
 	const { resizerURL: defaultResizerURL, resizerURLs } = getProperties(arcSite);
+	console.log("passed prop resizerURL", resizerURL);
 	console.log("resizerurls", resizerURLs);
 	console.log("ENVIRONMENT", ENVIRONMENT);
 	console.log("default resizer url", defaultResizerURL);
