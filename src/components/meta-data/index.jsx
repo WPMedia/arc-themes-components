5ba2dee6a207f389d1c1eaf3056978b7c9923f48
Blake Anderson <blake.anderson@washpost.com>
2025-08-27 16:30:07 -0400

    update for external_canonical_url field

--- src/components/meta-data/index.jsx
+++ src/components/meta-data/index.jsx
@@ -445,18 +445,23 @@ const MetaData = ({
 	);
 
 	if (outputCanonicalLink) {
-		const defaultResolution = getPageCanonicalUrl(
-			pageType,
-			canonicalDomain || websiteDomain,
-			gc,
-			requestUri,
-		);
-		const canonicalUrl = canonicalResolver
-			? canonicalResolver(pageType, defaultResolution)
-			: defaultResolution;
+		const externalCanonicalUrl = gc && gc.external_canonical_url;
+		if (externalCanonicalUrl) {
+			canonicalLink = <link rel="canonical" href={externalCanonicalUrl} />;
+		} else {
+			const defaultResolution = getPageCanonicalUrl(
+				pageType,
+				canonicalDomain || websiteDomain,
+				gc,
+				requestUri,
+			);
+			const canonicalUrl = canonicalResolver
+				? canonicalResolver(pageType, defaultResolution)
+				: defaultResolution;
 
-		if (canonicalUrl) {
-			canonicalLink = <link rel="canonical" href={canonicalUrl} />;
+			if (canonicalUrl) {
+				canonicalLink = <link rel="canonical" href={canonicalUrl} />;
+			}
 		}
 	}
 
